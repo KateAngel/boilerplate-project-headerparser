5402741a54f6acd8c498a3ed21371476ae7ecada
Kateryna <kvkyrpa@gmail.com>
2023-10-16 16:58:18 +0200

    feat: solution

--- index.js
+++ index.js
@@ -20,8 +20,12 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  let ipaddress = req.ip;
+  let language = req.headers['accept-language'];
+  let software = req.headers['user-agent'];
+
+  res.json({ ipaddress: ipaddress, language: language, software: software});
 });
 
 // listen for requests :)
